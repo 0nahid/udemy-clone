e1bf9084542e530cdf5ea2ae95a9b49ec0409938
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-08-20 20:27:05 +0600

    card added

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -1,29 +1,20 @@
 import React from 'react';
-import { Card, Button, Container, Row, Col, CardGroup } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
 
 const Course = (props) => {
     const { title, price, visible_instructors: { display_name: instructor }, image_480x270: img, } = props.course;
     const handleAddProduct = props.handleAddProduct;
     return (
-        <Container>
-            <Row>
-                <Col lg="4">
-                    <Card>
-                        <Card.Img variant="top" className="img-fluid" src={img} />
-                        <Card.Body>
-                            <Card.Title>{title}</Card.Title>
-                            <Card.Text>
-                                <p><small>By : {instructor} </small></p>
-                                <p>{price}</p>
-                            </Card.Text>
-                            <Button onClick={() => handleAddProduct(props.course)} variant="primary">Add to Cart</Button>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-        </Container>
-
+        <div>
+            <div className="course-details">
+                <img src={img} className="img-fluid" alt="" />
+                <h1>{title}</h1>
+                <p><small>By : {instructor} </small></p>
+                <p>{price}</p>
+                <button onClick={() => handleAddProduct(props.course)} className="btn btn-success">Add to cart</button>
+            </div>
+        </div>
     );
 };
 
-export default Course; 
+export default Course;
